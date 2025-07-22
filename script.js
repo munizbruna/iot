02504f42e86ad739688b71ef506f2ea12f71a279
munizbruna <bompatricio@gmail.com>
2025-07-22 10:38:04 -0300

    temperaturas a mais

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const temperaturas = [
-    {id: 1, temperatura:23.9},
-    {id: 2, temperatura:25.9},
-    {id: 3, temperatura:27.9},
-    {id: 4, temperatura:29.9}
+  {id: 1, temperatura: 23.4},
+  {id: 2, temperatura: 25.8},
+  {id: 3, temperatura: 21.7},
+  {id: 4, temperatura: 28.9},
+  {id: 5, temperatura: 30.2},
+  {id: 6, temperatura: 26.5},
+  {id: 7, temperatura: 24.1},
+  {id: 8, temperatura: 32.0},
+  {id: 9, temperatura: 27.3},
+  {id:10, temperatura: 29.6}
 ]
+
 
 function simularLeitura(){
     const sort = Math.floor(Math.random()* temperaturas.length)
